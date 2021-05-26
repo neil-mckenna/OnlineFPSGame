37c85300973415074da1442ec1f1347f5981eaee
TUFFY <emberhawk97@gmail.com>
2021-05-26 17:13:58 +0100

    created a scene with a player controller with some mouse look functionality

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Transform viewPoint;
+    public float mouseSensitivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            transform.rotation.eulerAngles.y + mouseInput.x, 
+            transform.rotation.eulerAngles.z);
+
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60, 60f);
+        
+        viewPoint.rotation = Quaternion.Euler(
+            verticalRotStore,
+            viewPoint.rotation.eulerAngles.y,
+            viewPoint.rotation.eulerAngles.z);
+
+        
+    }
+}
