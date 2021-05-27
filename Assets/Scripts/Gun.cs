ae568b10d7548be40a0dc1101cba8d10487dd24e
TUFFY <emberhawk97@gmail.com>
2021-05-27 16:04:54 +0100

    Created gun placeholder, added models and added gun script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gun : MonoBehaviour
+{
+    public bool isAutomatic;
+    public float timeBetweenShots = .1f, heatPerShot = 1f;
+
+
+}
