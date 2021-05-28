7023bd8fb2fe8ff7e814839bf3ffda34ce1cf0a1
TUFFY <emberhawk97@gmail.com>
2021-05-28 11:24:32 +0100

    Created the room browser logic

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -22,6 +22,10 @@ public class Launcher : MonoBehaviourPunCallbacks
     [Header("Error Settings")]
     public GameObject errorScreen;
     public TMP_Text errorText;
+    [Header("Browser Settings")]
+    public GameObject roomBrowserScreen;
+    public RoomButton theRoomButton;
+    private List<RoomButton> allRoomButtons = new List<RoomButton>();
 
 
     private void Awake() 
@@ -48,6 +52,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         createRoomScreen.SetActive(false);
         roomScreen.SetActive(false);
         errorScreen.SetActive(false);
+        roomBrowserScreen.SetActive(false);
     }
 
     public override void OnConnectedToMaster()
@@ -126,6 +131,47 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         CloseMenus();
         menuButtons.SetActive(true);
+
+    }
+
+    public void OpenRoomBrowser()
+    {
+        CloseMenus();
+        roomBrowserScreen.SetActive(true);
+    }
+
+    public void CloseRoomBrowser()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomButton rb in allRoomButtons)
+        {
+            Destroy(rb.gameObject);
+            
+        }
+        allRoomButtons.Clear();
+
+        theRoomButton.gameObject.SetActive(false);
+
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            if(roomList[i].PlayerCount != roomList[i].MaxPlayers && !roomList[i].RemovedFromList)
+            {
+                RoomButton newButton = Instantiate(theRoomButton, theRoomButton.transform.parent);
+                
+                newButton.SetButtonDetails(roomList[i]);
+                newButton.gameObject.SetActive(true);
+
+                allRoomButtons.Add(newButton);
+
+            }
+
+
+        }
 
 
     }
